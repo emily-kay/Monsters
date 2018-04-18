c872db50ada5ee5d1b3cceb68499324cd299e486
Emily Kay <emilykay917@gmail.com>
2018-04-18 17:09:30 -0500

    Server up and running, now ready for project

---
+++ server/public/scripts/client.js
@@ -0,0 +1,3 @@
+console.log('client.js is connected');
+
+$(document).ready(console.log('jquery is up and running'));
---
+++ server/server.js
@@ -0,0 +1,14 @@
+//Always necessary consts
+
+const express = require('express'); //translated: go to the node module folder, find express and run the code
+const app = express(); //the thing that was exported from express above, now run it
+const PORT = 5000; //all caps cause it won't change
+
+//Created consts
+
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`)
+}); //PORT is which entry way, can be any number between 1 and 9999, it's mostly arbitrary, occasionally it will be horrific
